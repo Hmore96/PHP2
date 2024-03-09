649b5ca48688efc43a61a3b1234adecd656923ba
Hmore96 <markus.haeckl@gmx.at>
2024-03-09 15:34:28 +0100

    Blabla

---
+++ admin/kopf.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rezepte-Verw.-Administrationsbereich</title>
+
+    <style>
+        body {
+            margin: 0px;
+            background: #ccd5ae;
+            color: #fefae0;
+        }
+        .row {background: #d4a373};
+    </style>
+</head>
+<body>
+    <nav>
+        <ul class="row">
+            <li><a href="index.php">Start</li>
+            <li><a href="zutaten_liste.php">Zutaten</li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
